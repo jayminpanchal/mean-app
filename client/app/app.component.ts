457d3a0d54341ec73ea6cda7ac0b457b12c2ce6c
Jaymin Panchal <jayminpanchal10@gmail.com>
2016-11-09 01:39:52 +0530

    Aded basic components

---
+++ client/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-app',
+    template: '<h1>Hello World</h1>'
+})
+
+export class AppComponent { }
