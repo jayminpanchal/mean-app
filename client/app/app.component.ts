6ad4c9e52808cbd525f3aae71fe1987d7b382a3a
Jaymin Panchal <jayminpanchal10@gmail.com>
2016-11-11 00:12:26 +0530

    Modified app.component and app.module

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {TaskService} from './services/task.service';
 
 @Component({
     moduleId: module.id,
     selector: 'my-app',
-    template: '<h1>Hello World</h1>'
+    templateUrl: 'app.component.html',
+    providers:[TaskService]
 })
 
 export class AppComponent { }
